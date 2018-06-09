64a24d3ed2b42b510a4e851e9e1565cf3cf2ba92
Tarik Mey <tarik-mey@gmx.de>
2018-06-09 10:36:24 +0200

    added employee schema

--- models.js
+++ models.js
@@ -1,4 +1,4 @@
-const uuid = require('uuid');
+/* const uuid = require('uuid');
 
 // employees model: manages employees
 const employeeList = {
@@ -20,7 +20,22 @@ const employeeList = {
         this.items[employee.id] = employee;
         return employee;
     },
-    // method 3: put endpoint to update an employee
+    // method 3: delete endpoint to delete an employee
+    delete: function(id) {
+        console.log(`Deleting employee \`${id}\``);
+        delete this.items[id];
+      },
+    // method 4: put endpoint to update an employee
+    update: function(updatedEmployee) {
+        console.log(`Deleting employee with id: \`${updatedEmployee.id}\``);
+        const {id} = updatedEmployee;
+        // if (!(id in this.items)) {
+        //     throw StorageException(
+        //     `Can't update employee \`${id}\` because doesn't exist.`)
+        // }
+        this.items[updatedEmployee.id] = updatedEmployee;
+        return updatedEmployee;
+    }
 };
 
 function createEmployeeList() {
@@ -32,3 +47,47 @@ function createEmployeeList() {
 module.exports = {
     employeeList: createEmployeeList()
 };
+
+ */
+
+// CREATING SCHEMA
+
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+
+// Schema to represent an employee
+/* const employeeSchema = mongoose.Schema({
+    employee: {
+        first_name: {type: String, required: true},
+        last_name: {type: String, required: true},
+    },
+    gender: {type: String, required: true},
+    hours: {type: Number, required: true},
+  }); */
+const employeeSchema = mongoose.Schema({
+    first_name: {type: String, required: true},
+    last_name: {type: String, required: true},
+    gender: {type: String, required: true},
+    hours: {type: Number, required: true},
+  });
+
+  employeeSchema.virtual('employeeName').get(function() {
+    // return `${this.employee.first_name} ${this.employee.last_name}`.trim();
+    return `${this.first_name} ${this.last_name}`.trim();
+  });
+  
+  employeeSchema.methods.serialize = function() {
+    return {
+      id: this._id,
+      employee: this.employeeName,
+      gender: this.gender,
+      hours: this.hours,
+    };
+  };
+
+const Employee = mongoose.model('Employee', employeeSchema);
+
+module.exports = {Employee};
