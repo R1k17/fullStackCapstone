a7b556724dcbe5eb0d0f926a2b1f0ca4000dc997
Tarik Mey <tarik-mey@gmx.de>
2018-06-09 10:37:51 +0200

    added runServer

--- server.js
+++ server.js
@@ -1,15 +1,50 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {PORT, DATABASE_URL} = require('./config');
 
 
 const employeeRouter = require('./employeeRouter');
 
+
+app.use(jsonParser);
 app.use(morgan('common'));
 app.use('/employees', employeeRouter);
 
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
 
 
-module.exports = app;
+
+let server;
+
+function runServer(databaseUrl, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+};
+
+module.exports = {runServer, app};
