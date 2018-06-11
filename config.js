ed16b4ede9510edcdf4984f69dd4a263cb335423
Tarik Mey <tarik-mey@gmx.de>
2018-06-11 19:10:02 +0200

    updated URL

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                      'mongodb://localhost/timePlanner-app';
-// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-//                       'mongodb://localhost/test-timePlanner-app';
+'use strict';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/timePlaner-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-timePlaner-app';
 exports.PORT = process.env.PORT || 8080;
