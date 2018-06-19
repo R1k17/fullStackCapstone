c5ce77ea83095e22cacaaaf54e98a8d45eef0a2a
Tarik Mey <tarik-mey@gmx.de>
2018-06-19 18:13:52 +0200

    updated employeeSchema.virtual

--- models.js
+++ models.js
@@ -1,55 +1,3 @@
-/* const uuid = require('uuid');
-
-// employees model: manages employees
-const employeeList = {
-    // method 1: get endpoint to retrieve all employees
-    get: function() {
-        console.log('Retrieving a list of all employees');
-        return Object.keys(this.items).map(key => this.items[key]);
-    },
-    // method 2: post endpoint to create a new employee
-    create: function(firstName, lastName, gender, hours) {
-        console.log('creating new employee');
-        const employee = {
-            id: uuid.v4(),
-            firstName: firstName,
-            lastName: lastName,
-            gender: gender,
-            hours: hours
-        };
-        this.items[employee.id] = employee;
-        return employee;
-    },
-    // method 3: delete endpoint to delete an employee
-    delete: function(id) {
-        console.log(`Deleting employee \`${id}\``);
-        delete this.items[id];
-      },
-    // method 4: put endpoint to update an employee
-    update: function(updatedEmployee) {
-        console.log(`Deleting employee with id: \`${updatedEmployee.id}\``);
-        const {id} = updatedEmployee;
-        // if (!(id in this.items)) {
-        //     throw StorageException(
-        //     `Can't update employee \`${id}\` because doesn't exist.`)
-        // }
-        this.items[updatedEmployee.id] = updatedEmployee;
-        return updatedEmployee;
-    }
-};
-
-function createEmployeeList() {
-    const storage = Object.create(employeeList);
-    storage.items = {};
-    return storage;
-}
-
-module.exports = {
-    employeeList: createEmployeeList()
-};
-
- */
-
 // CREATING SCHEMA
 
 'use strict';
@@ -57,6 +5,15 @@ module.exports = {
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+/* 
+const timeTableSchema = mongoose.Schema({
+  dayName: {type: String, required: true}
+})
+
+const shiftSchema = mongoose.Schema({
+  shiftName: {type: String, required: true}
+})
+ */
 
 // Schema to represent an employee
 /* const employeeSchema = mongoose.Schema({
@@ -76,11 +33,12 @@ const employeeSchema = mongoose.Schema({
 
   employeeSchema.virtual('employeeName').get(function() {
     // return `${this.employee.first_name} ${this.employee.last_name}`.trim();
-    return `${this.first_name} ${this.last_name}`.trim();
+    return `${this.first_name} + ' ' + ${this.last_name}`.trim();
   });
   
   employeeSchema.methods.serialize = function() {
     return {
+      // Do I want to display the ID for the user?
       id: this._id,
       employee: this.employeeName,
       gender: this.gender,
