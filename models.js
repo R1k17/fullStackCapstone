1bde87ea6149dd90ab8d64e47136a28bf0fe63a8
Tarik Mey <tarik-mey@gmx.de>
2018-06-22 06:56:31 +0200

    more details for employeeSchema

--- models.js
+++ models.js
@@ -41,6 +41,8 @@ const employeeSchema = mongoose.Schema({
       // Do I want to display the ID for the user?
       id: this._id,
       employee: this.first_name + ' ' + this.last_name,
+      first_name: this.first_name,
+      last_name: this.last_name,
       gender: this.gender,
       hours: this.hours,
     };
