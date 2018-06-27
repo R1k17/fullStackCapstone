2164a458ec69b975f94910d7499f79872caec90c
Tarik Mey <tarik-mey@gmx.de>
2018-06-27 20:08:40 +0200

    minor changes

--- public/timeTable_client.js
+++ public/timeTable_client.js
@@ -27,7 +27,7 @@ function displayAllTimeTables(data) {
 function renderTimeTable(result) {
     return `
     <div class="day border">
-        <p class="dayName border">${result.dayName}</p>
+        <h2 class="dayName border">${result.dayName}</h2>
         <div class="shifts"></div>
         <button class="addShift">Add shift +</button>
     </div>
@@ -40,6 +40,10 @@ function startApp() {
 
 startApp();
 
+// ==========================================================
+// ==== shift creation logic ====
+
+
 
 /* 
 ==== timetable creation ====
@@ -48,3 +52,49 @@ startApp();
     <button id="saveTimeTable" >Save timetable</button>
 </div>
  */
+
+
+ // ==========================================================
+//==== id logic ====
+let dayCounter = [0];
+let shiftCounter = 0;
+
+
+
+// 1. create timetable >> week gets loaded 
+const weekDay = 
+`<div class="day border">`+
+  '<p class="dayName border">Mo</p>' +
+  '<div class="shifts"></div>' +
+  '<button class="addShift">Add shift +</button>' +
+'</div>';
+
+// create day
+$('#createDay').on('click', function(){
+  //  > add 1 to dayCounter array
+  dayCounter.push('d'+dayCounter.length);
+  //  > check for last inserted day
+  $('.container').append(weekDay);
+  $('.container > div').last("div").attr('id', dayCounter[dayCounter.length-1]);
+  // it is changing the value of all buttons in every day container
+  // $('button').attr('value', dayCounter[dayCounter.length-1]);
+})
+
+// 2. inside the day create shifts
+const newShift = 
+'<span class="border shiftSlots">shift</span>';
+
+$('.container').on('click', '.addShift', function() {
+  // add 1 to shift counter
+  shiftCounter += 1;
+  // check the parent id of clicked button
+  let parentID = $(this).parent().attr('id');
+  console.log('parentID: '+ parentID);
+  // add shift if parentID matches parent of clicked button
+  if(parentID === $(this).parent().attr('id')) {
+    console.log(parentID + " vs. " + $(this).parent().attr('id'));
+    $('#' + parentID + '>.shifts').append(newShift);
+    // add shift ids 
+    // $('.shifts > span').last("span").attr('id', 'd'+dayCounter+'s'+shiftCounter);
+  }
+})
