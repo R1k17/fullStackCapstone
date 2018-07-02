f716fe323a0b2254858c018c580b1462eada7fbf
Tarik Mey <tarik-mey@gmx.de>
2018-07-02 08:27:55 +0200

    shift update func  added

--- shiftRouter.js
+++ shiftRouter.js
@@ -47,6 +47,28 @@ router.post('/', jsonParser, (req, res) => {
         });
 });
 
+router.put('/:id', (req,res) => {
+    if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        const message = (`Request path id (${req.params.id}) and request body id ` + `(${req.body.id}) must match`);
+        console.error(message);
+        return res.status(400).json({message: message});
+    }
+    const toUpdate = {};
+    const updateableFields = ['employee', 'start', 'end', 'hours'];
+
+    updateableFields.forEach(field => {
+        if(field in req.body) {
+            toUpdate[field] = req.body[field];
+        }
+    });
+
+    Shift
+        .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+        .then(shift => res.status(204).end())
+        .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+
 router.delete('/:id', jsonParser, (req,res) => {
     Shift
         .findByIdAndRemove(req.params.id)
