2a7b331ac970858f8511bbd50cc72aadf8b4043b
Tarik Mey <tarik-mey@gmx.de>
2018-07-13 18:17:26 +0200

    added JWT_SECRET and EXPIRY

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/timePlaner-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:0bananas@ds147190.mlab.com:47190/employees_tp';
+// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/timePlaner-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-timePlaner-app';
 exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'timer';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+
