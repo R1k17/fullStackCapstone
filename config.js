2b3501e96477688a38a09817e8cd8483acd37c37
Tarik Mey <tarik-mey@gmx.de>
2018-07-17 20:32:29 +0200

    changed database url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:0bananas@ds147190.mlab.com:47190/employees_tp';
-// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/timePlaner-app';
+// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:0bananas@ds147190.mlab.com:47190/employees_tp';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/timePlaner-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-timePlaner-app';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'timer';
