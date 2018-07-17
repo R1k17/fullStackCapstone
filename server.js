cef79c9d23df74d968363738908fb3cc311e0052
Tarik Mey <tarik-mey@gmx.de>
2018-07-17 08:55:25 +0200

    added auth functionality

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ let corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+	if (req.method === 'OPTIONS') {
+		return res.send(204);
+	}
+	next();
+});
+
 app.use(cors(corsOptions))
 
 app.use(jsonParser);
